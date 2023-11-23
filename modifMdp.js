69e15f142fc207d1e1d2f4b9cc749e414b1d9d71
Votre Nom <Vous@exemple.com>
2023-11-23 14:40:26 +0100

    V2 : les mdp sont vérifiés et doivent respecter des conditions pour eêtres soumis. NextStep : Ajouter du style

--- modifMdp.js
+++ modifMdp.js
@@ -1,5 +1,43 @@
 let resultat = document.querySelector("#resultat")
+let regexPwd = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
 
+// on gère la validité des mdps
+document.querySelector("#new-password1").addEventListener("keyup", (e) => {
+
+    if (!regexPwd.test(e.target.value)) {
+        e.target.style.backgroundColor = "red"
+    } else {
+        e.target.style.backgroundColor = "green"
+    }
+
+    if (regexPwd.test(e.target.value)
+        && regexPwd.test(document.querySelector("#new-password1").value)
+        && document.querySelector("#new-password2").value === document.querySelector("#new-password1").value) {
+        document.querySelector("#valider").disabled = false
+    } else {
+        document.querySelector("#valider").disabled = true
+    }
+
+})
+
+// on gère la validité des mdps
+document.querySelector("#new-password2").addEventListener("keyup", (e) => {
+    if (!regexPwd.test(e.target.value)) {
+        e.target.style.backgroundColor = "red"
+    } else {
+        e.target.style.backgroundColor = "green"
+    }
+
+    if (regexPwd.test(e.target.value)
+        && regexPwd.test(document.querySelector("#new-password1").value)
+        && document.querySelector("#new-password2").value === document.querySelector("#new-password1").value) {
+        document.querySelector("#valider").disabled = false
+    } else {
+        document.querySelector("#valider").disabled = true
+    }
+})
+
+// Gestion du submit du nouveau mdp
 document.querySelector("#change-password").onsubmit = (e) => {
     e.preventDefault()
     // Vérification de l'ancien mdp
@@ -8,8 +46,9 @@ document.querySelector("#change-password").onsubmit = (e) => {
     const pwd2 = document.querySelector("#new-password2").value
 
     // On vérifie si ils sont identiques
-    if (pwd1 === pwd2) {
+    if (pwd1 === pwd2 && pwd1 != "") {
         //Gestion de la validité du pwd si on a le temps
+
         // on construit le body
         let data = {
             token: localStorage.getItem("token"),
